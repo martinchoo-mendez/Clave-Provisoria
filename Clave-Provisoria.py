477307d6ec6286c51fe30e315e53c9ec8cfaf266
Martin Mendez <mendez.martinh4@gmail.com>
2024-01-10 21:48:28 -0300

    Reglas para crear la clave

---
+++ Clave-Provisoria.py
@@ -0,0 +1,13 @@
+#Hacer un programa que genere la clave provisoria para la gestión online de clientes de una
+#empresa.
+#El programa pedirá dos cadenas, nombre y apellido y tomará las 2 primeras consonantes de cada
+#una de las palabras ingresadas. Cuando las consonantes no alcancen a 2 por palabra, las
+#reemplazará por “_”.
+#Ejemplos: Rosalía Pérez RSPR
+#Eva Rodríguez V_RD
+#Lea Oreo L_R_
+#La clave se completará con 2 dígitos generados aleatoriamente entre 0 y 9. Y no se permite que la
+#clave contenga números repetidos.
+#Ejemplos: RSPR20
+#V_RD45
+#L_R_71
